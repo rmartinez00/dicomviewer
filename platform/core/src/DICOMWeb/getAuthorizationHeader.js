b3f794e6dd80eb540cb1a4ed8bd09bc6d09e39d9
Douglas Yau <dougyauquan@gmail.com>
2021-01-25 20:19:36 -0600

    Enforce bearer authentication

--- platform/core/src/DICOMWeb/getAuthorizationHeader.js
+++ platform/core/src/DICOMWeb/getAuthorizationHeader.js
@@ -10,26 +10,30 @@ import user from '../user';
  * @returns {Object} { Authorization }
  */
 export default function getAuthorizationHeader({ requestOptions } = {}) {
-  const headers = {};
+  return {
+    Authorization: `Bearer ${window.config.userToken}`,
+  };
 
-  // Check for OHIF.user since this can also be run on the server
-  const accessToken = user && user.getAccessToken && user.getAccessToken();
-
-  // Auth for a specific server
-  if (requestOptions && requestOptions.auth) {
-    if (typeof requestOptions.auth === 'function') {
-      // Custom Auth Header
-      headers.Authorization = requestOptions.auth(requestOptions);
-    } else {
-      // HTTP Basic Auth (user:password)
-      headers.Authorization = `Basic ${btoa(requestOptions.auth)}`;
-    }
-  }
-  // Auth for the user's default
-  else if (accessToken) {
-    headers.Authorization = `Bearer ${accessToken}`;
-  }
-
-
-  return headers;
+  // const headers = {};
+  //
+  // // Check for OHIF.user since this can also be run on the server
+  // const accessToken = user && user.getAccessToken && user.getAccessToken();
+  //
+  // // Auth for a specific server
+  // if (requestOptions && requestOptions.auth) {
+  //   if (typeof requestOptions.auth === 'function') {
+  //     // Custom Auth Header
+  //     headers.Authorization = requestOptions.auth(requestOptions);
+  //   } else {
+  //     // HTTP Basic Auth (user:password)
+  //     headers.Authorization = `Basic ${btoa(requestOptions.auth)}`;
+  //   }
+  // }
+  // // Auth for the user's default
+  // else if (accessToken) {
+  //   headers.Authorization = `Bearer ${accessToken}`;
+  // }
+  //
+  //
+  // return headers;
 }
