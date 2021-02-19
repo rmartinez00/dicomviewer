71e2712d99eb136f7be2fc43f009be016d49e1ce
Douglas Yau <dougyauquan@gmail.com>
2021-02-19 14:31:07 -0600

    Revert "fix: Add syncTranslations script"
    This reverts commit 9b606ff9eb5d31fe00158d4ed8fbb94e4852184f.

--- platform/i18n/src/locales/index.js
+++ platform/i18n/src/locales/index.js
@@ -1,23 +1,23 @@
-import de from './de/';
+import ar from './ar/';
 import en_US from './en-US/';
 import es from './es/';
 import fr from './fr/';
 import ja_JP from './ja-JP/';
 import nl from './nl/';
 import pt_BR from './pt-BR/';
-import ru from './ru/';
 import vi from './vi/';
 import zh from './zh/';
+import de from './de/';
 
 export default {
-  ...de,
+  ...ar,
   ...en_US,
   ...es,
+  ...de,
   ...fr,
   ...ja_JP,
   ...nl,
   ...pt_BR,
-  ...ru,
   ...vi,
   ...zh,
 };
--- platform/i18n/src/locales/pt-BR/index.js
+++ platform/i18n/src/locales/pt-BR/index.js
@@ -4,10 +4,7 @@ import CineDialog from './CineDialog.json';
 import Common from './Common.json';
 import DatePicker from './DatePicker.json';
 import Header from './Header.json';
-import MeasurementTable from './MeasurementTable.json';
-import StudyList from './StudyList.json';
 import UserPreferencesModal from './UserPreferencesModal.json';
-import ViewportDownloadForm from './ViewportDownloadForm.json';
 
 export default { 
   'pt-BR': {
@@ -17,9 +14,6 @@ export default {
     Common,
     DatePicker,
     Header,
-    MeasurementTable,
-    StudyList,
     UserPreferencesModal,
-    ViewportDownloadForm,
   }
 };
