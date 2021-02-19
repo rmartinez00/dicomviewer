b358bf2ea36bc534acf7653ffe2148b33487bed9
Douglas Yau <dougyauquan@gmail.com>
2021-02-19 14:31:07 -0600

    Revert "refactor: Update SidePanel.js to use a functional component (#2179)"
    This reverts commit dce852ef38adf9831943859d263e281d3d4f12c8.

--- platform/viewer/src/components/SidePanel.js
+++ platform/viewer/src/components/SidePanel.js
@@ -1,36 +1,41 @@
 import './SidePanel.css';
 
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
 
-const SidePanel = ({ from, isOpen, children, width }) => {
-  const fromSideClass = from === 'right' ? 'from-right' : 'from-left';
+class SidePanel extends Component {
+  static propTypes = {
+    from: PropTypes.string.isRequired,
+    isOpen: PropTypes.bool.isRequired,
+    children: PropTypes.node,
+    width: PropTypes.string,
+  };
 
-  const styles = width
-    ? {
-        maxWidth: width,
-        marginRight: isOpen ? '0' : Number.parseInt(width) * -1,
-      }
-    : {};
+  render() {
+    const fromSideClass =
+      this.props.from === 'right' ? 'from-right' : 'from-left';
 
-  return (
-    <section
-      style={styles}
-      className={classNames('sidepanel', fromSideClass, {
-        'is-open': isOpen,
-      })}
-    >
-      {children}
-    </section>
-  );
-};
+    const styles = this.props.width
+      ? {
+          maxWidth: this.props.width,
+          marginRight: this.props.isOpen
+            ? '0'
+            : Number.parseInt(this.props.width) * -1,
+        }
+      : {};
 
-SidePanel.propTypes = {
-  from: PropTypes.string.isRequired,
-  isOpen: PropTypes.bool.isRequired,
-  children: PropTypes.node,
-  width: PropTypes.string,
-};
+    return (
+      <section
+        style={styles}
+        className={classNames('sidepanel', fromSideClass, {
+          'is-open': this.props.isOpen,
+        })}
+      >
+        {this.props.children}
+      </section>
+    );
+  }
+}
 
 export default SidePanel;
